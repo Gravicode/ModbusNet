2ccb6bea907384747b000408589db300b4000b5e
muhammad ibnu fadhil <mifmasterz@gmail.com>
2022-12-11 08:24:52 +0700

    add

--- src/Modbus.Models/Model.cs
+++ src/Modbus.Models/Model.cs
@@ -1,12 +1,13 @@
 ﻿namespace Modbus.Models;
 public class DevicePerformance
 {
-    public int Id { get; set; }
     public float CpuUsage { get; set; }
     public float MemoryUsage { get; set; }
     public float CpuTemperature { get; set; }
-    public DateTime TimeStamp { get; set; }
     public float CpuHeat { get; set; }
+    public int Id { get; set; }
+    public DateTime TimeStamp { get; set; }
+
 }
 
 
