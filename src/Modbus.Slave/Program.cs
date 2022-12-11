ec3030ab60a85e851f5112bdd157cddb776fb7a0
muhammad ibnu fadhil <mifmasterz@gmail.com>
2022-12-11 08:10:24 +0700

    add

--- src/Modbus.Slave/Program.cs
+++ src/Modbus.Slave/Program.cs
@@ -1,8 +1,13 @@
-﻿using Modbus.Slave;
+﻿using Modbus.Shared;
+using Modbus.Slave;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddSingleton<IModbusServer>(new ModbusServer());
+        services.AddSingleton<IModbusClient>(new ModbusClient());
+        services.AddSingleton<IPiDevicePerformanceInfo>(new PiDevicePerformanceInfo());
+
         services.AddHostedService<Worker>();
     })
     .Build();
